b572fc1e4dde4509808fcecb9db7b8cfe6cfecf5
Kristof Bach <crys@crys.hu>
2025-12-31 18:53:26 +0100

    Simplify entrypoint, make it POSIX compatible

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,25 @@
-#!/bin/bash
-
-# Initialize command
-cmd="web-indexer"
+#!/bin/sh
 
 # Check each expected environment variable and append it to the command if set
-[[ -n "$BASE_URL" ]] && cmd="$cmd --base-url \"$BASE_URL\""
-[[ -n "$CONFIG" ]] && cmd="$cmd --config \"$CONFIG\""
-[[ -n "$DATE_FORMAT" ]] && cmd="$cmd --date-format \"$DATE_FORMAT\""
-[[ "$DIRS_FIRST" == "true" ]] && cmd="$cmd --dirs-first"
-[[ -n "$INDEX_FILE" ]] && cmd="$cmd --index-file \"$INDEX_FILE\""
-[[ "$LINK_TO_INDEX" == "true" ]] && cmd="$cmd --link-to-index"
-[[ -n "$LOG_LEVEL" ]] && cmd="$cmd --log-level \"$LOG_LEVEL\""
-[[ "$MINIFY" == "true" ]] && cmd="$cmd --minify"
-[[ -n "$NOINDEX_FILES" ]] && cmd="$cmd --noindex-files \"$NOINDEX_FILES\""
-[[ -n "$SKIPINDEX_FILES" ]] && cmd="$cmd --skipindex-files \"$SKIPINDEX_FILES\""
-[[ -n "$ORDER" ]] && cmd="$cmd --order \"$ORDER\""
-[[ "$RECURSIVE" == "true" ]] && cmd="$cmd --recursive"
-[[ -n "$SKIP" ]] && cmd="$cmd --skip \"$SKIP\""
-[[ -n "$SORT" ]] && cmd="$cmd --sort \"$SORT\""
-[[ -n "$SOURCE" ]] && cmd="$cmd --source \"$SOURCE\""
-[[ -n "$TARGET" ]] && cmd="$cmd --target \"$TARGET\""
-[[ -n "$TEMPLATE" ]] && cmd="$cmd --template \"$TEMPLATE\""
-[[ -n "$THEME" ]] && cmd="$cmd --theme \"$THEME\""
-[[ -n "$TITLE" ]] && cmd="$cmd --title \"$TITLE\""
-
-# Debug: Print the command to be executed
-echo "Executing command: $cmd"
+[ -n "$BASE_URL" ] && set -- "$@" --base-url "$BASE_URL"
+[ -n "$CONFIG" ] && set -- "$@" --config "$CONFIG"
+[ -n "$DATE_FORMAT" ] && set -- "$@" --date-format "$DATE_FORMAT"
+[ "$DIRS_FIRST" = "true" ] && set -- "$@" --dirs-first
+[ -n "$INDEX_FILE" ] && set -- "$@" --index-file "$INDEX_FILE"
+[ "$LINK_TO_INDEX" = "true" ] && set -- "$@" --link-to-index
+[ -n "$LOG_LEVEL" ] && set -- "$@" --log-level "$LOG_LEVEL"
+[ "$MINIFY" = "true" ] && set -- "$@" --minify
+[ -n "$NOINDEX_FILES" ] && set -- "$@" --noindex-files "$NOINDEX_FILES"
+[ -n "$SKIPINDEX_FILES" ] && set -- "$@" --skipindex-files "$SKIPINDEX_FILES"
+[ -n "$ORDER" ] && set -- "$@" --order "$ORDER"
+[ "$RECURSIVE" = "true" ] && set -- "$@" --recursive
+[ -n "$SKIP" ] && set -- "$@" --skip "$SKIP"
+[ -n "$SORT" ] && set -- "$@" --sort "$SORT"
+[ -n "$SOURCE" ] && set -- "$@" --source "$SOURCE"
+[ -n "$TARGET" ] && set -- "$@" --target "$TARGET"
+[ -n "$TEMPLATE" ] && set -- "$@" --template "$TEMPLATE"
+[ -n "$THEME" ] && set -- "$@" --theme "$THEME"
+[ -n "$TITLE" ] && set -- "$@" --title "$TITLE"
 
 # Execute the constructed command
-eval $cmd $@
+exec web-indexer "$@"
